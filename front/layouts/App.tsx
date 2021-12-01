d944a5f0a74fea08eca6ddbcfe5a082a6eb5ab8d
punkryn <gns816715@gmail.com>
2021-12-01 15:50:18 +0900

    react router dom

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import SignUp from '@pages/SignUp';
+import Login from '@pages/Login';
 
 const App = () => {
-  return <div>wait!!!!!!!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
